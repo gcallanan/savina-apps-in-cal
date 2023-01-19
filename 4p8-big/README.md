2fd8ba95102a40ddd93b9982412fcc97e579d86d
Gareth Callanan <gcallanan@gmail.com>
2023-01-19 15:10:18 +0100

    More modifications added.

--- 4p8-big/README.md
+++ 4p8-big/README.md
@@ -1,3 +1,9 @@
+# General Outline
+
+Big is a benchmark consisting of N messenger actors. Each messenger actor has a connection to every other messenger actor. A messenger sends a ping out on a random port and waits for a pong response from the same port. Each messenger performs P pings. A messenger waits for a pong response before sending the next ping.
+
+The benchmark is configured by adjusting N and P.
+
 # Requirements
 1. [streamblocks-tycho](https://git.cs.lth.se/dataflow/streamblocks-tycho) on the LU gitlab servers has been installed and you are using branch "EntityRenamingBug" branch. We use the the tychoc binary in the streamblocks-tycho repo, not the streamblocks binary in the [streamblocks-platforms](https://git.cs.lth.se/dataflow/streamblocks-tycho/-/tree/EntityRenamingBug) repo to build.
 
@@ -5,6 +11,11 @@
 From within the 4P8-BIG directory
 1. mkdir target
 2. time tychoc  --set pretty-print-post-template-sub=off --set pretty-print-test=off  --set experimental-network-elaboration=on --source-path . --target-path target big.BigNetwork && cc target/*.c -o add && time ./add testOut
+
+Alternativly:
+- Running `python3 runtests.py` will run a script that runs over a range of N and P values and generates CSV files describing total compilation and running times.
+- Running `python3 compileTimeMeasurements.py` will run a script that runs over a range of N values and generating a CSV file describing the compilation times for different phases giving more granular insight into the compilation process.
+
 
 # Potential Issues
 1. The template substitition results in a new set of source code being generated for each actor. This may not always be necessary and can result in lots of code being generated.
--- 4p8-big/runtests.py
+++ 4p8-big/runtests.py
@@ -1,15 +1,30 @@
+"""
+@author: Gareth Callanan
+
+Script that runs the BIG benchmark for multiple tests.
+
+Takes about 1200 seconds to run on a 12 core (6 real, 12 virtual) laptop when
+numTests=3, N_values=2..9 and P_values=1000..10000000.
+"""
+
+
 import os
 import time
 import struct
+import csv
 
 # 1. Set up
-N_values = [2,3,4,5,6]
-P_values = [1000,10000, 100000,1000000,10000000]
-numTests = 1
+N_values = [2, 3, 4, 5, 6, 7, 8, 9]
+P_values = [1000, 10000, 100000, 1000000, 10000000]
+numTests = 3
 totalTests = numTests * len(P_values) * len(N_values)
 
-timesCompilation_s = [[ [0]*numTests for i in range(len(P_values))] for j in range(len(N_values))]
-timesRuntime_s = [[ [0]*numTests for i in range(len(P_values))] for j in range(len(N_values))]
+timesCompilation_s = [
+    [[0] * numTests for i in range(len(P_values))] for j in range(len(N_values))
+]
+timesRuntime_s = [
+    [[0] * numTests for i in range(len(P_values))] for j in range(len(N_values))
+]
 
 totalStartTime_s = time.time()
 
@@ -18,7 +33,7 @@ testsDone = 0
 for testIndex in range(numTests):
     for N_index in range(len(N_values)):
         for P_index in range(len(P_values)):
-            
+
             N = N_values[N_index]
             P = P_values[P_index]
             # 2.1. Set config.cal
@@ -33,41 +48,45 @@ for testIndex in range(numTests):
             f.write(configFileContents)
             f.close()
 
-
             # 2.2. remove and recreate target directory
-            os.system("rm -rf target && mkdir target")
+            retCode = os.system("rm -rf target && mkdir target")
+            if retCode != 0:
+                raise Exception(f"Return code equal to {retCode}, expected 0")
 
             # 2.3. Build and compile
             start = time.time()
-            os.system("tychoc --set experimental-network-elaboration=on --source-path . --target-path target big.BigNetwork && cc target/*.c -o calBinary")
+            retCode = os.system(
+                "tychoc --set experimental-network-elaboration=on --source-path . --target-path target big.BigNetwork && cc target/*.c -o calBinary"
+            )
+            if retCode != 0:
+                raise Exception(f"Return code equal to {retCode}, expected 0")
             compileTime_s = time.time() - start
 
             # 2.4. Run
             start = time.time()
-            os.system("./calBinary testOut")
+            retCode = os.system("./calBinary testOut")
+            if retCode != 0:
+                raise Exception(f"Return code equal to {retCode}, expected 0")
             runTime_s = time.time() - start
-
-            #print(f"For N = {N}, P = {P}:")
-            #print(f"\t{compileTime_s} compile time")
-            #print(f"\t{runTime_s} run time")
 
             timesCompilation_s[N_index][P_index][testIndex] = compileTime_s
             timesRuntime_s[N_index][P_index][testIndex] = runTime_s
 
             # 2.5 Check the value of testOut
-            with open("testOut", mode='rb') as file: # b is important -> binary
+            with open("testOut", mode="rb") as file:
                 fileContent = file.read()
-                checksum = struct.unpack("i",fileContent)
-                if(checksum[0] != N):
-                    raise Exception(f"Checksum value: {checksum[0]}, expected {N}") 
+                checksum = struct.unpack("i", fileContent)
+                if checksum[0] != N:
+                    raise Exception(f"Checksum value: {checksum[0]}, expected {N}")
 
             testsDone += 1
-            print(f"Test {testsDone} of {totalTests} ({round((testsDone)/totalTests*100,2)}) done. N={N}, P={P}, repeat={testIndex}. Compile time {round(compileTime_s,2)}s. Runtime: {round(runTime_s,2)}s.")
+            print(
+                f"Test {testsDone} of {totalTests} ({round((testsDone)/totalTests*100,2)}%) done. N={N}, P={P}, repeat={testIndex}. Compile time {round(compileTime_s,2)}s. Runtime: {round(runTime_s,2)}s."
+            )
 
 
 # 3. Reset config file to prevent having to recommit to git
-configFileContents = f"""
-namespace big:
+configFileContents = """namespace big:
     uint numMessengers = 5;
     uint numPingPongs = 1000000;
 end
@@ -77,17 +96,44 @@ f = open("config.cal", "w")
 f.write(configFileContents)
 f.close()
 
-# 4. Work out all the averages 
-avgCompileTime_s = [ [0]*len(P_values) for i in range(len(N_values))]
-avgRuntime_s = [ [0]*len(P_values) for i in range(len(N_values))]
+# 4. Work out all the averages
+avgCompileTime_s = [[0] * len(P_values) for i in range(len(N_values))]
+avgRuntime_s = [[0] * len(P_values) for i in range(len(N_values))]
 
 for N_index in range(len(N_values)):
     for P_index in range(len(P_values)):
-        avgCompileTime_s[N_index][P_index] = sum(timesCompilation_s[N_index][P_index])/numTests # Can be done with numpy, but I am keeping the external libraries to a minimal
-        avgRuntime_s[N_index][P_index] = sum(timesRuntime_s[N_index][P_index])/numTests
+        # Can be done with numpy, but I am keeping the external libraries to a minimal
+        avgCompileTime_s[N_index][P_index] = (
+            sum(timesCompilation_s[N_index][P_index]) / numTests
+        )
+        avgRuntime_s[N_index][P_index] = (
+            sum(timesRuntime_s[N_index][P_index]) / numTests
+        )
 
 # 5. Write to CSV
 
+# 5.1 Append row and column labels
+rowLabels = N_values
+
+# 5.1.1 New row label column
+for row, label in zip(avgRuntime_s, rowLabels):
+    row.insert(0, label)
+
+for row, label in zip(avgCompileTime_s, rowLabels):
+    row.insert(0, label)
+
+# 5.1.2 New column label row
+avgRuntime_s = [["N\\P"] + P_values] + avgRuntime_s
+avgCompileTime_s = [["N\\P"] + P_values] + avgCompileTime_s
+
+# 5.2 Write to file
+with open("runtimes.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(avgRuntime_s)
+
+with open("compileTimesCoarse.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(avgCompileTime_s)
 
 # 6. Done
 print(f"Total Time: {round(time.time()-totalStartTime_s,2)} s")
