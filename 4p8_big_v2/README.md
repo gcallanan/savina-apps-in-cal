bdc1c24779a0994a2576aab4117288336091ae40
Gareth Callanan <gmcallanan@gmail.com>
2023-07-06 11:06:25 +0200

    Added all big v2 files

---
+++ 4p8_big_v2/README.md
@@ -0,0 +1,18 @@
+# Benchmark 4.8 in the Savina paper: Big
+
+## General Outline
+
+Big is a benchmark consisting of N messenger actors. Each messenger actor has a connection to every other messenger actor. A messenger sends a ping out on a random port and waits for a pong response from the same port. Each messenger performs P pings. A messenger waits for a pong response before sending the next ping.
+
+The benchmark is configured by adjusting N and P.
+
+A Savina implementation of this benchmark can be found in the [BigHabaneroActorBenchmark.scala](https://github.com/shamsimam/savina/blob/master/src/main/scala/edu/rice/habanero/benchmarks/big/BigHabaneroActorBenchmark.scala) with configuration parameters for this benchmark available in [BigConfig.java](https://github.com/shamsimam/savina/blob/master/src/main/java/edu/rice/habanero/benchmarks/big/BigConfig.java)
+
+[BigNetwork.cal](./BigNetwork.cal) describes the overall network for the benchmark.
+
+Compared to [Big_v1](../4p8_big_v1/), this version of Big has more actions in each messenger actor but when converted to an Actor Machine, the maximum messenger actors size compilable tends to be bigger.
+
+## Quick Run
+From within the 4P8-big directory:
+1. mkdir build
+2. time tychoc --set experimental-network-elaboration=on --source-path . --target-path build big.BigNetwork && cc build/*.c -o add && time ./add testOut
---
+++ scripts/big_4p8_v2.py
@@ -0,0 +1,69 @@
+import benchmark
+from typing import List
+import struct
+
+
+class big_4p8_v2(benchmark.Benchmark):
+    def __init__(self):
+        self.__DIRECTORY__ = "4p8_big_v2"
+        self.__TOP_ACTOR_NAME__ = "big.BigNetwork"
+        self.__BENCHMARK_NAME__ = "big"
+        self.__XCLBIN_NAME__ = "BigNetworkStreamblocks_kernel_xclbin"
+        self.__TOP_ACTOR_NAME_NO_PREPATH__ = "BigNetwork"
+        self.__TOP_ACTOR_NAME_STREAMBLOCKS_SUFFIX__ = "Streamblocks"
+
+    def getAMScalingParameters(self) -> List[tuple]:
+        parameters = [
+            ("N", [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13 ,14, 15, 16, 17, 18, 19, 20, 22, 24, 26, 28, 30, 35, 40, 45, 50, 55, 60]),
+            # ("N", [2, 3, 4, 5, 6]),
+            #("N", [2, 3]),
+        ]
+        return parameters
+
+    def getBuildParameters(self) -> List[tuple]:
+        parameters = self.getAMScalingParameters()
+
+        parameters = parameters + [
+            ("P", [1000, 10000, 100000, 1000000, 10000000]),
+        ]
+        return parameters
+
+    def getConfigFileContents(self, **kwargs) -> str:
+        N = kwargs["N"]
+        P = kwargs["P"] if "P" in kwargs else 1000
+
+        configFileContents = f"""namespace big:
+    uint numMessengers = {N};
+    uint numPingPongs = {P};
+end
+"""
+        return configFileContents
+
+    # Need something to confirm correctness
+    def confirmRuntime(self, **kwargs) -> bool:
+        N = kwargs["N"]
+        P = kwargs["P"]
+
+        with open(self.__DIRECTORY__ + "/data/out", mode="rb") as file:
+            fileContent = file.read()
+            checksum = struct.unpack("i", fileContent)
+            if checksum[0] != N:
+                raise Exception(
+                    f"N={N}, P={P}. Checksum value: {checksum[0]}, expected {N}"
+                )
+
+        return True
+
+    def getInputFiles(self) -> List[str]:
+        return []
+
+    def getOutputFiles(self) -> List[str]:
+        retList = ["out"]
+        retList = [self.__DIRECTORY__ + "/data/" + s for s in retList]
+        return retList
+
+    def getDependentVariableKey(self) -> str:
+        return "P"
+    
+    def getScalingActorName(self) -> str:
+        return "Messenger"
